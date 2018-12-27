5f3c6150d2e1d03f1a14847fdd26e9593b10d19b
caijq <caiiiac@163.com>
2018-12-27 16:04:21 +0800

    result

---
+++ app/src/main/java/com/simple/android/jdmallsimple/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.simple.android.jdmallsimple.activity;
+
+import android.os.Bundle;
+
+import com.simple.android.jdmallsimple.R;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void initUI() {
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
