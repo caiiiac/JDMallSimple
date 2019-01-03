805ab3a739ffb9731c1af4046b68ba2fbf01f771
caijq <caiiiac@163.com>
2019-01-03 16:18:30 +0800

    add category controller

---
+++ app/src/main/java/com/simple/android/jdmallsimple/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package com.simple.android.jdmallsimple.controller;
+
+import android.content.Context;
+
+public class CategoryController extends BaseController {
+
+    public CategoryController(Context c) {
+        super(c);
+    }
+
+    @Override
+    protected void handleMessage(int action, Object... values) {
+
+    }
+}
