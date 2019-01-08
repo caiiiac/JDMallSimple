51b477bb1bc0288b5ff42814207a4b665f293850
caijq <caiiiac@163.com>
2019-01-08 17:04:55 +0800

    add 商品详情相关

---
+++ app/src/main/java/com/simple/android/jdmallsimple/activity/ProductDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.simple.android.jdmallsimple.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.simple.android.jdmallsimple.R;
+
+public class ProductDetailsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
---
+++ app/src/main/java/com/simple/android/jdmallsimple/fragment/ProductDetailsFragment.java
@@ -0,0 +1,30 @@
+package com.simple.android.jdmallsimple.fragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.simple.android.jdmallsimple.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProductDetailsFragment extends Fragment {
+
+
+    public ProductDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_details, container, false);
+    }
+
+}
