4f62101329fa670aa56b1abef5b7379c39465049
caijq <caiiiac@163.com>
2019-01-10 16:56:55 +0800

    商品详情

--- app/src/main/java/com/simple/android/jdmallsimple/fragment/ProductDetailsFragment.java
+++ app/src/main/java/com/simple/android/jdmallsimple/fragment/ProductDetailsFragment.java
@@ -6,8 +6,12 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.simple.android.jdmallsimple.R;
+import com.simple.android.jdmallsimple.activity.ProductDetailsActivity;
+import com.simple.android.jdmallsimple.cons.NetworkConst;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,20 +19,31 @@ import com.simple.android.jdmallsimple.R;
 public class ProductDetailsFragment extends BaseFragment {
 
 
-    public ProductDetailsFragment() {
-        // Required empty public constructor
-    }
-
+    private WebView mWebView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_details, container, false);
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_product_details,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initUI();
     }
 
     @Override
     protected void initUI() {
-
+        mWebView =(WebView) getActivity().findViewById(R.id.webview);
+        ProductDetailsActivity activity = (ProductDetailsActivity) getActivity();
+        mWebView.loadUrl(NetworkConst.PRODUCTDETAIL_URL+"?productId="+activity.mProductId);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mWebView.getSettings().setJavaScriptEnabled(true);
     }
 }
